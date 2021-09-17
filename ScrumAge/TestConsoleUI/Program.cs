90c016bec5b77836e95f3ef341036196228fbea5
scottcd <SCOTTCD1@ETSU.EDU>
2021-09-17 14:48:14 -0400

    Setup folders

--- ScrumAge/TestConsoleUI/Program.cs
+++ ScrumAge/TestConsoleUI/Program.cs
@@ -3,7 +3,7 @@ using GameLibrary;
 
 namespace TestConsoleUI {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args) {s
             Fish fish = new Fish();
             Console.WriteLine($"Hello {fish.Name}. You weight {fish.Weight}lbs.");
         }
