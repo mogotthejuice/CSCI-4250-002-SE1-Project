5e4d2c57f168c7041ae72e0fdec798c836e09531
scottcd <SCOTTCD1@ETSU.EDU>
2021-09-20 18:45:03 -0400

    Added GameController Class. This is the class that will interact with the UI's

---
+++ ScrumAge/GameLibrary/Services/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Services {
+    public static class GameController {
+        /// <summary>
+        /// Initialization class. Locations and Resource Board will be initialized here.
+        /// </summary>
+        public static void Init() { 
+            
+        }
+    }
+}
