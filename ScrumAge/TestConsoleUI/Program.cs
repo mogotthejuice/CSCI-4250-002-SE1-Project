8523190021dbab81d7e142a84eff0789d2024a26
SethNorton06 <sethnorton06@gmail.com>
2021-09-20 13:02:27 -0400

    started working on the UI

--- ScrumAge/TestConsoleUI/Program.cs
+++ ScrumAge/TestConsoleUI/Program.cs
@@ -5,7 +5,6 @@ namespace TestConsoleUI {
     class Program {
         static void Main(string[] args) {
             Fish fish = new Fish();
-            Resources.resources = new 
             Console.WriteLine($"Hello {fish.Name}. You weight {fish.Weight}lbs.");
         }
     }
