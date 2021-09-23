50c791e9ffa974e7b0ca3e442777a78cac3be3b6
Drew Whitinger <WHITINGERA@ETSU.EDU>
2021-09-23 02:00:36 -0400

    Add ConsultantCards, LicenseTiles, Overclocks to Player class.

--- ScrumAge/GameLibrary/Models/Overclock.cs
+++ ScrumAge/GameLibrary/Models/Overclock.cs
@@ -7,21 +7,22 @@ using System.Threading.Tasks;
 namespace GameLibrary.Models {
     public class Overclock {
         private const int START_LEVEL = 1;
-        private const int MAX_LEVEL = 4;
+
+        public const int MAX_LEVEL = 4;
 
         public int Level { get; set; }
         public bool HasBeenUsed { get; private set; }
 
         public void Upgrade() {
             if (Level == MAX_LEVEL)
-                throw new InvalidOperationException("Cannot upgrade max level Overclock.")
+                throw new InvalidOperationException("Cannot upgrade max level Overclock.");
 
             Level++;
         }
 
         public void Use() {
             if (HasBeenUsed)
-                throw new InvalidOperationException("Cannot use Overclock that has already been used this turn.")
+                throw new InvalidOperationException("Cannot use Overclock that has already been used this turn.");
             
             HasBeenUsed = true;
         }
