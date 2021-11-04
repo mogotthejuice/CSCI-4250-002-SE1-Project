fe32fecef34145e85052f1c6debb0f16e12c3742
BMartinson <martinson2000@gmail.com>
2021-11-03 22:20:08 -0400

    Update and rename UpperCardEnums to UpperCardEnums.cs
    I made the file a cs file so it is an actual piece of code, and fixed the white space.

---
+++ ScrumAge/GameLibrary/Models/Cards/UpperCardEnums.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GameLibrary.Models;
+
+namespace GameLibrary.Models 
+{
+    public enum UpperEnum
+    {
+        DICE_ROLL,
+        RESOURCE,
+        RESOURCE_DICE_ROLL,
+        VICTORY_POINTS,
+        OVERCLOCK,
+        BITCOIN_INVESTMENT,
+        CONSULTANT_CARD,
+        ONE_USE_OVERCLOCK,
+        EXTRA_TOOL_TILE,
+        AGRICULTURE_SPACE
+    }
+}
