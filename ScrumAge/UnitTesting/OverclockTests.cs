3caf6b25acf115de75ce47d838c6506a3c530838
Martinson_GIT <martinson2000@gmail.com>
2021-11-19 17:04:13 -0500

    Update OverclockTests.cs
    Cleaned unnecessary whitespace and added in the needed [TestFicture] tag on line 12.

--- ScrumAge/UnitTesting/OverclockTests.cs
+++ ScrumAge/UnitTesting/OverclockTests.cs
@@ -9,11 +9,9 @@ using GameLibrary.Models;
 
 namespace UnitTesting 
 {
-
-    
+    [TestFixture]
     public class OverclockTests
     {
-
         [SetUp]
         public void setUp()
         {
@@ -22,7 +20,6 @@ namespace UnitTesting
         }
 
         [Test]
-
         public void Upgrade_isAtMaxLevel_NoUpgrade()
         {
             var overclock = new GameLibrary.Models.Overclock();
@@ -31,14 +28,13 @@ namespace UnitTesting
             int maxValue = 4;
 
             //Verify that level is not at Maximum level
-                if (startlevel == maxValue)
-                {
-                //Act + Assert
-                Assert.Throws<InvalidOperationException>(() => overclock.Upgrade());
-
-                }
+            if (startlevel == maxValue) {
+            //Act + Assert
+            Assert.Throws<InvalidOperationException>(() => overclock.Upgrade());
+            }
         }
-         [ Test]
+        
+        [Test]
         public void Upgrade_isNotAtMaxLevel_Upgraded()
         {
             //Arrange
@@ -48,74 +44,55 @@ namespace UnitTesting
             int maxValue = 4;
 
             //Verify that level is not at Maximum level
-            if (startlevel < maxValue)
-            {
+            if (startlevel < maxValue) {
                 //Act
                 startlevel++;
-                
-
             }
+            
             //Assert
             Assert.AreNotEqual(startlevel,maxValue);
-
-
-          }
-         [ Test]    
+        }
+        
+        [Test]    
         public void Overclock_hasBeenUsed_UseOperationException()
-         {
+        {
             //initialize variable
             bool HasBeenUsed = true;
             //Arrange
             var overclock = new GameLibrary.Models.Overclock();
             //Act
             overclock.Use();
-           if( HasBeenUsed )
-           { 
-            //Assert
-             Assert.Throws<InvalidOperationException>(() => overclock.Use());
-           }
-
+            if(HasBeenUsed) { 
+                //Assert
+                Assert.Throws<InvalidOperationException>(() => overclock.Use());
+            }
         }
-     [Test]  
-     public void Overclock_hasNotBeenUsed_Use()
-         {
-              
+        
+        [Test]  
+        public void Overclock_hasNotBeenUsed_Use()
+        {
             //Arrange
             var overclock = new Overclock();
             //Act
             overclock.Use();
-            
             //Assert
-             Assert.IsFalse(overclock.HasBeenUsed);
-
-
+            Assert.IsFalse(overclock.HasBeenUsed);
         }
-    [Test]
-    public void Overclock_Reset()
-         {
-              
+        
+        [Test]
+        public void Overclock_Reset()
+        {      
             //initialize variable
             bool HasBeenUsed = false;
             //Arrange
             var overclock = new GameLibrary.Models.Overclock();
             //Act
             overclock.Reset();
-           if( HasBeenUsed )
-           { 
-             //Assert
-             Assert.IsFalse(overclock.HasBeenUsed);
-           }
             
-        
-
+            if(HasBeenUsed) { 
+                //Assert
+                Assert.IsFalse(overclock.HasBeenUsed);
+            }
         }
-        
-
-
-
     }
-
-    
-
-    
 }
