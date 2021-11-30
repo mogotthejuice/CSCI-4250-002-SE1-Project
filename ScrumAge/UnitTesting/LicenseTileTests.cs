10051ddac82cb5ec035967ba69ca2582d19dd96e
Martinson_GIT <martinson2000@gmail.com>
2021-11-29 23:02:33 -0500

    Update LicenseTileTests.cs
    Fixed minor typos and cleaned whitespace.

--- ScrumAge/UnitTesting/LicenseTileTests.cs
+++ ScrumAge/UnitTesting/LicenseTileTests.cs
@@ -9,96 +9,81 @@ using GameLibrary.Services;
 using GameLibrary.Models;
 
 
-namespace UnitTests
+namespace UnitTesting
 {
     [TestFixture]
     class LicenseTileTest
     {
-       public Dictionary<Resources, int> resources { get; private set; } 
-       private LicenseTile tile;
+        public Dictionary<Resources, int> resources { get; private set; } 
+        private LicenseTile tile;
        
         [SetUp]
         public void Setup()
         {
             tile = new LicenseTile();
-
         }
 
         [Test]
         public void InadequateResourceUsage_toPurchaseLicenseTile_Exception()
         {
-           //Act and Assert
-             if (tile.Type == LicenseTileType.FIXED_RESOURCES)
-              {
+            //Act and Assert
+            if (tile.Type == LicenseTileType.FIXED_RESOURCES)
+            {
                 foreach (Resources resource in tile.RequiredResources.Keys)
                     if (tile.RequiredResources[resource] != resources[resource]) 
                     {
                         Assert.Throws<InvalidOperationException>(() =>tile.GetScore(resources));
                     }
-                 
-              }
-
-           
-           
-
+            }
         }
+        
         [Test]
         public void SpecificPlayerChosenResources_equals_NecessaryResource_And_TotalResourcesUsed_isEquivalentTo_DeterminedAmountOfResources_Exception()
         {
-             bool valid = false;
-           //Act and Assert
-           if (tile.Type == LicenseTileType.FIXED_NUM_RESOURCES & HelperFunctions.TotalNumResources(resources) != tile.NumResources)
-            {
-              
-                Assert.Throws<InvalidOperationException>(() =>tile.GetScore(resources));
-                    
-                 
+            bool valid = false;
+            //Act and Assert
+            if (tile.Type == LicenseTileType.FIXED_NUM_RESOURCES & HelperFunctions.TotalNumResources(resources) != tile.NumResources)
+            {  
+                Assert.Throws<InvalidOperationException>(() =>tile.GetScore(resources));    
             }
             else
             {
                 //player chooses X number resources of Y different types
                 //Act and Assert
-                    tile.GetScore(resources);
-                    int numTypes = 0;
-                    foreach (int value in resources.Values)
-                        if (value > 0)
-                            numTypes++;
-                            valid= true;
-
-                // clarifies that the resource type selected by user matches the available resource types for the game
-                    if (numTypes != tile.NumResourceTypes)
+                tile.GetScore(resources);
+                int numTypes = 0;
+                foreach (int value in resources.Values)
+                    if (value > 0)
                     {
-                        Assert.Throws<InvalidOperationException>(() =>tile.GetScore(resources));
-                        Assert.IsFalse(valid);
-
+                        numTypes++;
+                        valid= true;
                     }
-                     Assert.AreEqual(numTypes, tile.NumResourceTypes);
-
-
-
-
-            }
-
-           
-           
-
-        }
-         [Test]
-         public void Verfication_Of_UtilizedResources()
-         {
-           //Arrange
-            var resource = tile.GetScore(resources);
-           //Act and Assert
-            int MAX_RESOURCES_USED = 7;
-            int numResources = HelperFunctions.TotalNumResources(resources);
-                if (numResources > MAX_RESOURCES_USED || numResources <= 0)
+                
+                // clarifies that the resource type selected by user matches the available resource types for the game
+                if (numTypes != tile.NumResourceTypes)
                 {
                     Assert.Throws<InvalidOperationException>(() =>tile.GetScore(resources));
-                }
-                   
-                 
-              Assert.IsNotNull(resource);
-          }
+                    Assert.IsFalse(valid);
 
+                }
+                Assert.AreEqual(numTypes, tile.NumResourceTypes);
+            }
+        }
+        
+        [Test]
+        public void Verfication_Of_UtilizedResources()
+        {
+            //Arrange
+            var resource = tile.GetScore(resources);
+            //Act and Assert
+            int MAX_RESOURCES_USED = 7;
+            int numResources = HelperFunctions.TotalNumResources(resources);
+            if (numResources > MAX_RESOURCES_USED || numResources <= 0)
+            {
+                Assert.Throws<InvalidOperationException>(() =>tile.GetScore(resources));
+            }       
+                 
+            Assert.IsNotNull(resource);
+        }
     }
 }
